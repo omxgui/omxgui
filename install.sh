33e87020bcb9619880426c2f626c456843c8bf7c
omxgui <mail@omxgui.com>
2016-07-26 19:31:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ sudo cp -r resources/omxguirpi /etc
 sudo chmod -R 777 /etc/omxguirpi
 
 
-sudo cp resources/bin/omxgui /bin/omxgui
-sudo chmod +x /bin/omxgui
+sudo cp resources/bin/omxgui /bin/omxguirpi
+sudo chmod +x /bin/omxguirpi
 
 sudo cp resources/desktop/omxgui.desktop /usr/share/raspi-ui-overrides/applications/omxgui.desktop
 
